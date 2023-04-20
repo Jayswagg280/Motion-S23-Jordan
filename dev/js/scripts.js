33aebe3728ee2ea6baddaf93b9a09d3eee5fb8dc
Jayswagg280 <102743923+Jayswagg280@users.noreply.github.com>
2023-04-20 18:07:38 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,7 +2,7 @@ import { gsap } from "gsap";
 
 gsap.from("#logo", {duration:1, alpha:0, delay:0.5})
 gsap.from("#nav li", {duration:1, alpha:0, ease: "power2.out", y:"-=25", delay:0.5})
-gsap.from("#hero-content", {duration:1, alpha:0, ease: "power2.out", scale: 0.75, delay:1.5})
+gsap.from("#hero-content", {duration:1.5, alpha:0, ease: "power2.out", scale: 0.75, delay:1.5})
 gsap.from("#hero article", {duration:2, alpha:0, ease: "power2.out", delay:0.75})
 gsap.from("#book-covers", {duration:2, alpha:0, ease: "power2.out", y:"+=35", delay:1.75})
 gsap.utils.toArray("img").forEach(img => {
