892a2bd44bbbc82030bcb576eb7e19db1a1de3ba
Jayswagg280 <102743923+Jayswagg280@users.noreply.github.com>
2023-04-20 15:22:28 -0400

    updated animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,12 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+gsap.from("#logo", {duration:1, alpha:0, delay:0.5})
+gsap.from("#nav li", {duration:1, alpha:0, ease: "power2.out", y:"-=25", delay:0.5})
+gsap.from("#hero-content", {duration:1, alpha:0, ease: "power2.out", scale: 0.75, delay:1.5})
+gsap.from("#hero article", {duration:2, alpha:0, ease: "power2.out", delay:0.75})
+gsap.from("#book-covers", {duration:2, alpha:0, ease: "power2.out", y:"+=35", delay:1.75})
+gsap.utils.toArray("img").forEach(img => {
+    let hover = gsap.to(img, {scale: 1.12, duration: 1.5, paused: true, ease: "power1.inOut"});
+    img.addEventListener("mouseenter", () => hover.play());
+    img.addEventListener("mouseleave", () => hover.reverse());
+  });
