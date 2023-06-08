6b3048c82cb83010d4ac7a61450bf44885ed783b
Jayswagg280 <102743923+Jayswagg280@users.noreply.github.com>
2023-06-08 15:10:58 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,64 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+// import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+// import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MotionPathPlugin);
+
+
+//INIT
+gsap.set("#closed", {scale:4});
+gsap.set("#open", {scale:4});
+gsap.set("#iris", {scale:4});
+//gsap.set("#t-path", {autoAlpha:0});
+
+
+function eyeAnimation(){
+    let tl = gsap.timeline();
+
+    tl.from("#closed", {duration:0.75, autoAlpha:0, ease:"none"})
+    tl.from("#open", {duration:0.75, autoAlpha:0, ease:"none"})
+    tl.from("#iris", {duration:0.5, autoAlpha:0, ease:"none"},"-=20%")
+    tl.to("#closed", {duration:0.5, autoAlpha:0, ease:"none"},"-=20%")
+    tl.to("#closed", {duration:0.2, scale:1, ease:"none", delay:1})
+    tl.to("#open", {duration:0.2, scale:1, ease:"none"})
+    tl.to("#iris", {duration:0.2, scale:1, ease:"none"},"<-=")
+    ;
+
+
+    return tl;
+
+
+}
+
+function Animation(){
+  let tl = gsap.timeline();
+
+  tl.from("#closed", {duration:0.75, autoAlpha:0, ease:"none"})
+  tl.from("#open", {duration:0.75, autoAlpha:0, ease:"none"})
+  tl.from("#iris", {duration:0.5, autoAlpha:0, ease:"none"},"-=20%")
+  tl.to("#closed", {duration:0.5, autoAlpha:0, ease:"none"},"-=20%")
+
+  ;
+
+
+  return tl;
+
+
+}
+
+
+
+
+
+
+let mainTl = gsap.timeline();
+mainTl.add(eyeAnimation())
+
+;
+
+
+
+
+// GSDevTools.create();
