c08ac05f3b9e5b4c58c1aa31c2df81bdb41c4736
Jayswagg280 <102743923+Jayswagg280@users.noreply.github.com>
2023-06-08 00:34:38 -0400

    logo in

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,30 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+// import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+// import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin, MotionPathHelper);
+
+//INIT
+
+function eyeAnimation(){
+  let tl = gsap.timeline();
+  tl.from("#closed",{duration:0.25, autoAlpha:0})
+  
+  ;
+
+  return tl;
+
+}
+
+
+let mainTl = gsap.timeline();
+mainTl.add(eyeAnimation())
+    
+
+;
+
+//GSDevTools.create();
+
+// tl.to("",{})  
+// tl.from("",{})
